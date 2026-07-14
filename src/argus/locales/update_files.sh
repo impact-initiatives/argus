698669ccf15bfe38e55a268bf01d464d80d91423
Greg <gregory.sabella@impact-initiatives.org>
2026-07-14 14:29:26 +0300

    fix translation script

--- src/argus/locales/update_files.sh
+++ src/argus/locales/update_files.sh
@@ -4,7 +4,7 @@ cd "$SCRIPT_DIR"
 
 set -euo pipefail
 
-find locales -type f -name "*.po" -print0 | xargs -0 -I {} sh -c '
+find . -type f -name "*.po" -print0 | xargs -0 -I {} sh -c '
     po_file="{}"
     mo_file="${po_file%.po}.mo"
     msgfmt -o "$mo_file" "$po_file"
